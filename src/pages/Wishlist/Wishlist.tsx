9eb07190037bbd35d52ef1a5203989b54310bd2b
Jerrell Marvel <jerrell.marvel88@gmail.com>
2022-05-04 10:35:35 +0700

    add wishlist page

--- src/pages/Wishlist/Wishlist.tsx
+++ src/pages/Wishlist/Wishlist.tsx
@@ -3,8 +3,9 @@ function Wishlist() {
     <div className="pt-20 text-center">
       <h2 className="my-4 text-3xl font-medium uppercase">Wishlist</h2>
 
-      <div>
-        <h3>My Wishlist</h3>
+      <div className="bg-slate-50 p-4 text-center ">
+        <h3 className="text-2xl font-medium">My Wishlist</h3>
+        <p>No products added to wishlist</p>
       </div>
     </div>
   );
